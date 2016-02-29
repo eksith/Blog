129614ac647819c6905098017e6c40f6f964b4fe
Eksith Rodrigo <eksith@live.com>
2016-02-29 02:00:58 -0500

    Added files via upload

---
+++ vendor/Blog/Messaging/Request.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Blog\Messaging;
+
+class Request extends Message 
+	implements \Psr\Http\Message\RequestInterface {
+	
+	protected $method;
+	protected $target;
+	protected $uri;
+	protected $sent_headers;
+	
+	public function __construct(
+		$method,
+		Uri $uri,
+		$body,
+		array $headers,
+		$protocol
+	) {
+		parent::__construct( $body, $protocol, $headers );
+		$this->method	= strtoupper( $method );
+		$this->uri	= $uri;
+	}
+	
+	public function getRequestTarget() {
+		if ( isset( $this->target ) ) {
+			return $this->target;
+		}
+		$target	= $this->uri->getPath();
+		if ( empty( $target ) ) {
+			$target = '/';
+		}
+		$query	= $this->uri->getQuery();
+		if ( !empty( $query ) ) {
+			$target .= '?' . $query;
+		}
+		
+		$this->target = $target;
+		return $target;
+	}
+	
+	public function withRequestTarget( $requestTarget ) {
+		return static::immu( $this, 'target', $requestTarget );
+	}
+	
+	public function getMethod() {
+		return $this->method;
+	}
+	
+	public function withMethod( $method ) {
+		return 
+		static::immu( $this, 'method', strtoupper( $method ) );
+	}
+	
+	public function getUri() {
+		return $this->uri;
+	}
+	
+	public function withUri(
+		\Psr\Http\Message\UriInterface $uri, 
+		$preserveHost = false 
+	) {
+		if ( $uri === $this->uri ) {
+			return $this;
+		}
+		
+		$new		= clone $this;
+		$new->uri	= $uri;
+		
+		if ( $preserveHost ) {
+			return $new;
+		}
+		if ( $host = $uri->getHost() ) {
+			$new->updateFromUri( $host );
+		}
+		
+		return $new;
+	}
+	
+	private function updateFromUri( $host ) {
+		if ( $port = $this->uri->getPort() ) {
+			$host .= ':' . $port;
+		}
+		$this->loadHeader( 'host', 'Host', $host );
+	}
+}
