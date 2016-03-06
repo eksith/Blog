947dd24c4ea3d69d694711498957a02aad9738b8
Eksith Rodrigo <eksith@live.com>
2016-03-06 14:37:34 -0500

    Create Login.php

---
+++ vendor/Blog/Views/User/Login.php
@@ -0,0 +1,12 @@
+<?php
+namespace Blog\Views\User;
+use Blog\Events;
+use Blog\Views;
+
+class Login extends Views\View {
+	
+	public function loginView( Events\Event $event ) {
+		# TODO
+		echo 'login view';
+	}
+}
