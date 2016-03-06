93201aa00b68152bfead0f4ebfc44436e8df0670
Eksith Rodrigo <eksith@live.com>
2016-03-06 14:37:54 -0500

    Create Register.php

---
+++ vendor/Blog/Views/User/Register.php
@@ -0,0 +1,12 @@
+<?php
+namespace Blog\Views\User;
+use Blog\Events;
+use Blog\Views;
+
+class Register extends Views\View {
+	
+	public function registering( Events\Event $event ) {
+		# TODO
+		echo 'registering view';
+	}
+}
