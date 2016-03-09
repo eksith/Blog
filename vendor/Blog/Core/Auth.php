86c532d76c36bd3ca4b739801d370f3c4fd18b0f
Eksith Rodrigo <eksith@live.com>
2016-03-09 00:08:32 -0500

    Update Auth.php

--- vendor/Blog/Core/Auth.php
+++ vendor/Blog/Core/Auth.php
@@ -10,13 +10,6 @@ class Auth extends Handlers\Handler  {
 	
 	public function __construct( Events\Dispatcher $dispatcher ) {
 		parent::__construct( $dispatcher );
-		/*
-		$session = new BlogSession();
-		\session_set_save_handler( $session, true );
-		register_shutdown_function( 
-			'session_write_close' 
-		);
-		*/
 	}
 	
 	/**
@@ -27,17 +20,13 @@ class Auth extends Handlers\Handler  {
 	 * Handles all events
 	 */
 	public function handleEvent( Events\Event $event ) {
-		$this->sessionCheck();
 		$this->processEventRules(
 			$event->getName(), 
 			$event->getRules()
 		);
 		
-		$event->set( 'session_id', session_id() );
-	}
-	
-	private function processEventRules( $name, $rules ) {
-		
+		$event->set( 'session_id', $_SESSION['canary']['visit'] );
+		$event->set( 'user_id', 0 );
 	}
 	
 	/**
@@ -67,72 +56,7 @@ class Auth extends Handlers\Handler  {
 		}
 	}
 	
-	/**
-	 * First visit session initialization
-	 */
-	private function session( $reset = false ) {
-		if ( 
-			\session_status() === \PHP_SESSION_ACTIVE && 
-			!$reset 
-		) {
-			return;
-		}
-		
-		if ( \session_status() != \PHP_SESSION_ACTIVE ) {
-			session_start();
-		}
-		if ( $reset ) {
-			\session_regenerate_id( true );
-			foreach ( array_keys( $_SESSION ) as $k ) {
-				unset( $_SESSION[$k] );
-			}
-		}
-	}
-	
-	/**
-	 * Check session staleness
-	 */
-	private function sessionCheck( $reset = false ) {
-		if ( !isset( $_SESSION['canary'] ) || $reset ) {
-			$this->session( true );
-			$this->sessionCanary();
-			return;
-		}
-		
-		if ( 
-			strcmp( 
-				$_SESSION['canary']['sig'], 
-				$this->getSignature() 
-			) !== 0 
-		) {
-			$this->session( true );
-			$this->sessionCanary();
-			return;
-		}
-		
-		if ( 
-			$_SESSION['canary']['exp'] < time() - 
-			$this->getSetting( 'session_time' )
-		) {
-			$this->session();
-			\session_regenerate_id( true );
-			$this->sessionCanary();
-		}
-	}
-	
-	/**
-	 * Session owner and staleness marker
-	 * 
-	 * @link https://paragonie.com/blog/2015/04/fast-track-safe-and-secure-php-sessions
-	 */
-	private function sessionCanary() {
-		$key	= $this->getSetting( 'visit_key' );
-		$bytes	= $this->getCrypto()->bytes( $key );
-		
-		$_SESSION['canary'] = [
-			'exp'	=> time(),
-			'visit'	=> bin2hex( $bytes ),
-			'sig'	=> $this->getSignature()
-		];
+	private function processEventRules( $name, $rules ) {
+		# TODO
 	}
 }
