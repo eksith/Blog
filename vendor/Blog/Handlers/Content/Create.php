d186f58490c8f12b478d32a91fc1e3dcbbb50c64
Eksith Rodrigo <eksith@live.com>
2016-03-09 00:13:10 -0500

    Update Create.php

--- vendor/Blog/Handlers/Content/Create.php
+++ vendor/Blog/Handlers/Content/Create.php
@@ -3,14 +3,15 @@
 namespace Blog\Handlers\Content;
 use Blog\Handlers;
 use Blog\Events;
+use Blog\Models;
 
 class Create extends Handlers\Handler {
 	
 	private $filter = array(
-		'csrf'		=> \FILTER_SANITIZE_ENCODED,
+		'csrf'		=> \FILTER_SANITIZE_STRING,
 		'parent'	=> \FILTER_VALIDATE_INT,
-		'title'		=> \FILTER_SANITIZE_ENCODED,
-		'publish'	=> \FILTER_SANITIZE_ENCODED,
+		'title'		=> \FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+		'publish'	=> \FILTER_SANITIZE_STRING,
 		'summary'	=> \FILTER_UNSAFE_RAW,
 		'body'		=> \FILTER_UNSAFE_RAW,
 		'status'	=> 
@@ -34,9 +35,44 @@ class Create extends Handlers\Handler {
 	}
 	
 	public function createPost( Events\Event $event ) {
-		# TODO
 		$data = filter_input_array( \INPUT_POST, $this->filter );
+		$csrf = $this->verifyCsrf( 
+				$data['csrf'], 'newpost', $event 
+			);
 		
-		var_dump( $data );
+		if ( $csrf ) {
+			$this->save( $data, $event );
+		} else {
+			$this->redirect( '/', 401 );
+		}
+	}
+	
+	private function save( $data, Events\Event $event ) {
+		$filter			= $this->getHtmlFilter();
+		$post			= new Models\Post();
+		
+		$post->title		= empty( $data['title'] ) ?
+			'Untitled' : $data['title'];
+		
+		$post->raw		= empty( $data['body'] ) ? 
+			'' : $data['body'];
+		
+		$post->summary		= empty( $data['summary'] ) ? 
+			'' : $filter->clean( $data['summary'], false );
+		
+		$post->body		= $filter->clean( $post->raw );
+		$post->plain		= strip_tags( $post->body );
+		$post->parent_id	= empty( $data['parent'] ) ?
+			0 : abs( ( int ) $data['parent'] );
+		
+		$post->user_id		= $event->get( 'user_id' );
+		$post->save();
+		if ( $post->id ) {
+			$this->redirect( '/manage/edit/' . $post->id, 201 );
+		} else {
+			# This is terrible
+			# TODO Some error handling
+			$this->redirect( '/' );
+		}
 	}
 }
