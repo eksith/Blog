30651ec3156c684d78716426892dc82b62a78b5e
Eksith Rodrigo <eksith@live.com>
2016-03-10 00:12:56 -0500

    Update Index.php

--- vendor/Blog/Views/Content/Index.php
+++ vendor/Blog/Views/Content/Index.php
@@ -12,10 +12,44 @@ class Index extends Views\View {
 	}
 	
 	public function index( Events\Event $event ) {
-		echo 'index view ';
+		$conds		= array();
+		
+		$title		= 'test heading';
+		$vars		= 
+		array(
+			'heading'	=> $title,
+			'page_title'	=> $title,
+			'host_uri'	=> $_SERVER['SERVER_NAME'],
+			'theme'		=> $this->getThemeDisplay()
+		);
+		
+		
+		$threads	= 
+		array(
+			array(
+				'post_author'	=> 'author',
+				'post_title'	=> 'test title',
+				'date_u'	=> 'date_u',
+				'date'		=> 'date',
+				'post_id'	=> '12',
+				'post_tags'	=> 'tags'
+			)
+		);
+		$this->addState( 'threads', $threads );
+		echo $this->sendView( 'forum.html', $conds, $vars );
 	}
 	
 	public function archive( Events\Event $event ) {
+		$this->menuBuilder( $event, $conds );
+		
 		echo 'archive view ';
+	}
+	
+	public function viewPosts( Events\Event $event ) {
+		$vars	= array(
+			'csrf'		=> $event->get( 'searchpost_csrf' )
+		);
+		
+		$event->set( 'search_form', $vars );
 	}
 }
