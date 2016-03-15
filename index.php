e2d318a265531c8d79ebedc53ed35fe78e15c65c
Eksith Rodrigo <eksith@live.com>
2016-03-15 00:09:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
-define( 'START',		-microtime( true ) );
+# Execution start (from request time)
+define( 'START',		isset( $_SERVER['REQUEST_TIME'] ) ? 
+					$_SERVER['REQUEST_TIME'] : 
+					microtime( true ) );
 
-// The following files should ideally be placed outside the web root
+# The following files should ideally be placed outside the web root
 require( 'config.php' );
 require( 'routes.php' );
+
+# Uncomment to test execution time
+# echo 'Ran in ' . round( microtime( true ) - START, 5 ) . ' seconds';
