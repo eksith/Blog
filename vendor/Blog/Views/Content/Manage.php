2f66d348a22c9b6a65066edbee6670e685cad8bd
Eksith Rodrigo <eksith@live.com>
2016-03-17 02:53:46 -0400

    Update Manage.php

--- vendor/Blog/Views/Content/Manage.php
+++ vendor/Blog/Views/Content/Manage.php
@@ -15,11 +15,16 @@ class Manage extends Views\View {
 	}
 	
 	public function viewPosts( Events\Event $event ) {
-		$cond = array();
-		$vars	= array(
+		$cond	= array();
+		$lang	= $event->get( 'lang' );
+		
+		$vars	= 
+		array(
 			'page_title'	=> 'Post list',
 			'page_heading'	=> 'Blog',
-			'theme'		=> $this->getThemeDisplay()
+			'theme'		=> $this->getThemeDisplay(),
+			'copyright'	=> 
+				$lang->term( 'copyright', date( 'Y' ) )
 		);
 		
 		$vars	= array_merge( $vars, $event->get( 'search_form' ) );
@@ -27,44 +32,61 @@ class Manage extends Views\View {
 	}
 	
 	public function creatingPost( Events\Event $event ) {
-		$cond = array(
+		$cond	= 
+		array(
 			'editor'	=> 'create'
 		);
-		$vars	= array(
+		$lang	= $event->get( 'lang' );
+		
+		$vars	= 
+		array(
 			'page_title'	=> 'New post',
 			'page_heading'	=> 'Blog',
-			'theme'		=> $this->getThemeDisplay()
+			'theme'		=> $this->getThemeDisplay(),
+			'copyright'	=> 
+				$lang->term( 'copyright', date( 'Y' ) )
 		);
 		
 		$vars	= array_merge( $vars, $event->get( 'create_form' ) );
-		echo $this->sendView( 'manage_newpost.html', $cond, $vars );
+		echo $this->sendView( 'manage_posteditor.html', $cond, $vars );
 	}
 	
 	public function editingPost( Events\Event $event ) {
-		$cond = array(
+		$cond	= 
+		array(
 			'editor'	=> 'edit'
 		);
-		$vars = array(
+		$lang	= $event->get( 'lang' );
+		
+		$vars	=
+		array(
 			'page_title'	=> 'Editing post',
 			'page_heading'	=> 'Blog',
-			'theme'		=> $this->getThemeDisplay()
+			'theme'		=> $this->getThemeDisplay(),
+			'copyright'	=> 
+				$lang->term( 'copyright', date( 'Y' ) )
 		);
 		
 		$vars = array_merge( $vars, $event->get( 'edit_form' ) );
-		echo $this->sendView( 'manage_editpost.html', $cond, $vars );
+		echo $this->sendView( 'manage_posteditor.html', $cond, $vars );
 	}
 	
 	public function deletingPost( Events\Event $event ) {
-		$cond = array(
+		$cond	= 
+		array(
 			'editor'	=> 'delete'
 		);
-		$vars = array(
+		$lang	= $event->get( 'lang' );
+		
+		$vars	= 
+		array(
 			'page_title'	=> 'Deleting post',
 			'page_heading'	=> 'Blog',
-			'theme'		=> $this->getThemeDisplay()
+			'theme'		=> $this->getThemeDisplay(),
+			'copyright'	=> $lang->term( 'copyright', 2016 )
 		);
 		
 		$vars = array_merge( $vars, $event->get( 'delete_form' ) );
-		echo $this->sendView( 'manage_content.html', $cond, $vars );
+		echo $this->sendView( 'manage_posteditor.html', $cond, $vars );
 	}
 }
