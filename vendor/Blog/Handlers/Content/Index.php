423ca23c4650e99e8298e2b5b3092f092f969656
Eksith Rodrigo <eksith@live.com>
2016-03-22 05:18:39 -0400

    Update Index.php

--- vendor/Blog/Handlers/Content/Index.php
+++ vendor/Blog/Handlers/Content/Index.php
@@ -1,13 +1,25 @@
 <?php
 
 namespace Blog\Handlers\Content;
+use Blog\Models;
 use Blog\Handlers;
 use Blog\Events;
 
 class Index extends ContentHandler {
 	
 	public function index( Events\Event $event ) {
-		# echo 'home controller ';
+		$page		= $event->get( 'page' );
+		$page		= empty( $page ) ? 1 : $page;
+		
+		$posts		= 
+		Models\Post::find( array( 
+			'search'	=> 'all',
+			'values'	=> 'all',
+			'page'		=> $page,
+			'fields'	=> 'summary'
+		) );
+		
+		$event->set( 'posts', $posts );
 	}
 	
 	public function archive( Events\Event $event ) {
