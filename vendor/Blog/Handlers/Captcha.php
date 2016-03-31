367b8a04d663d24b5f9d9384117a666c5cd650ad
Eksith Rodrigo <eksith@live.com>
2016-03-30 22:00:25 -0400

    Create Captcha.php

---
+++ vendor/Blog/Handlers/Captcha.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Blog\Handlers;
+
+class Captcha extends Handler {
+	
+	
+	public function generate( Events\Event $event ) {
+		# TODO
+	}
+	
+	public function validate( Events\Event $event ) {
+		# TODO
+	}
+}
